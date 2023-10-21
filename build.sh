3c52ccba2e07d78c8afb9237ebcbca89d068da8e
Yutsing Kuh <yutsing@outlook.com>
2023-10-21 16:49:11 +0800

    Optimize build process: reduce temporary file size; Remove files before some process

--- build.sh
+++ build.sh
@@ -7,6 +7,9 @@ if [ ! -d $target ]; then
   mkdir $target
 fi
 
+# 删除目标文件夹中的全部文件
+find $target -type f -delete
+
 # 遍历当前目录和子目录下的所有.ly文件
 find . -type f -name "*.ly" | while read -r file; do
   # 执行 lilypond 命令生成PDF文件
@@ -16,13 +19,13 @@ done
 # 优化生成的target文件夹中的PDF文件
 find $target -type f -name "*.pdf" | while read -r pdf_file; do
   # 生成优化后的PDF文件，覆盖原始文件
-  pdf2ps "$pdf_file" "$pdf_file.temp"
-  ps2pdf "$pdf_file.temp" "$pdf_file"
-  rm "$pdf_file.temp"
+  pdf2ps "$pdf_file" - | ps2pdf - "$pdf_file.temp"
+  mv "$pdf_file.temp" "$pdf_file"
 done
 
 # 删除target文件夹中的全部MIDI文件
 find $target -type f -name "*.midi" -delete
 
 # 压缩 $target 文件夹的为Scores.zip
+rm Scores.zip
 zip -r Scores.zip $target
